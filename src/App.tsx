d169c84502314ae0b3b6055e830a4913c083f469
Laura Murria <laura.murria@nesta.org.uk>
2022-02-02 15:41:30 +0000

    Skeleton with simple input, call to dummy model, display result

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import './App.css';
 import React from "react";
 import { useState } from "react";
 import { Report } from './Report';
-import { InputParams } from './Input';
-import { predict, Result } from './model';
+import { InputForm } from './InputForm';
+import { InputParams, predict, Result } from './model';
+import { Alert, Box, Grid, StyledEngineProvider } from '@mui/material';
 
 export default function App() {
 
-  const [ input, setInput ] = useState(null as InputParams | null);
   const [ result, setResult ] = useState(null as Result | null );
   const [ error, setError ] = useState(null as string | null);
 
@@ -26,136 +26,46 @@ export default function App() {
 
   const reset = () => {
     setError(null);
-    setInput(null);
     setResult(null);
   };
-
-
-
-  const handleSubmitUsageInfo = (usage: UsageInfo) => {
-    setError(null);
-    if (!usage.value && !(usage.value > 0) ) {
-      setError('Please check you entered your gas usage');
-    } else { 
-      setError(null);
-      let usageVal;
-      switch (String(usage.period)) {
-        case Period.Week: {
-            usageVal = usage.value * 52;
-            break;
-        }
-        case Period.Month: {
-          usageVal = usage.value * 12;
-          break;
-        }
-        case Period.Quarter: {
-          usageVal = usage.value * 4;
-          break;
-        }
-        case Period.Year: {
-            usageVal = usage.value;
-            break;
-        }
-        default: {
-            setError('Invalid usage period');
-            return;
-        }
-      }
-      if (usage.units !== Unit.kWh) {
-        // unit will be £ so convert to kwh
-        // deduct average annual charge then divide by average price per kwh
-        if (usageVal < 100) {
-          setError('Amount seems too low - just your standing charge will make your bill £7/month or more');
-          return;
-        }
-        usageVal = (usageVal - 94.81)/0.034;
-      }
-
-      const carbon = calculateCarbon(usageVal);
-      if (carbon < 1100 ||  carbon > 10000 ) 
-        setError(`This seems unusual, check your figure, is it definitely in ${usage.units} and for ${usage.units}`);     
-      setCarbon(carbon);
-      const equivalents = 
-        calculateEquivalents(carbon);
-      setApplyReduction(false);
-      setEquivalents(equivalents);
-      setError(null);
-    }
-  }
+  
 
   return ( 
     <StyledEngineProvider injectFirst>
-      <Box sx={{ border: 3, padding: 5, minHeight: 400 }} 
-       className={applyReduction ? "darkBackground" : "lightBackground"}>
+      <Box sx={{ border: 3, padding: 5, minHeight: 400 }} >
       {error ? <Alert severity="error">{error}</Alert> : null}
-      {/* If carbon is not yet known, this is the start - collect usage info, or allow flag unknown usage*/}
-      { (!carbon)
+      This is my app
+      {
+      // If result is not yet known, this is the start - collect input 
+      (!result)
         ?
-          // If usage flagged as unknown, collect premises info to enable emissions estimation
-          (!usageUnknown) 
-          ?
-            <Grid container spacing={2} flexWrap='wrap'>
-              <Grid item xs={12} sm={6}>
-                <Grid item xs={12}>
-                  <h1>How much gas do you use?</h1>
-                  <h3>Enter the information from your latest bill or smart meter</h3>
-                </Grid>
-                <Grid item xs={12} >
-                  <p>&nbsp;</p>
-                  <p>&nbsp;</p>
-                  <p>&nbsp;</p>
-                  <p>&nbsp;</p>
-                </Grid>
-                <Grid item xs={12} >
-                  <Button className="btn btn--primary" variant="contained" onClick={() => flagUsageUnknown(true)}>Help me estimate</Button>
-                </Grid>
+          <Grid container spacing={2} flexWrap='wrap'>
+            <Grid item xs={12} sm={6}>
+              <Grid item xs={12}>
+                <h1>My overall heading</h1>
+                <h3>Tell us some information and we'll give you something back</h3>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                  <InputUsage
-                      handleSubmitUsageInfo={handleSubmitUsageInfo}
-                    />
+              <Grid item xs={12} >
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
               </Grid>
             </Grid>
-          : 
-            <Grid container spacing={2} flexWrap='wrap'>
-              <Grid item xs={12} sm={6}>
-                <Grid item xs={12}>
-                  <h1>How much gas do you use?</h1>
-                  <h3>Tell us about your property and we'll estimate</h3>
-                </Grid>
-                <Grid item xs={12} >
-                  <p>&nbsp;</p>
-                  <p>&nbsp;</p>
-                  <p>&nbsp;</p>
-                  <p>&nbsp;</p>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button className="btn btn--primary" variant="contained" onClick={() => flagUsageUnknown(false)}>I know my usage</Button>
-                </Grid>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <EstimateUsage onSubmit={handleSubmitPremisesInfo}/>
-              </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputForm handleSubmit={handleSubmitInput}/>
             </Grid>
+          </Grid>
         : 
         <>
-          {/* Once stats are present show report */}
-          { (equivalents)
+          {/* Once result is present show report */}
+          { (result)
           ?
-            /* Once user has clicked to apply the reduction show report with reduction */
-            (!applyReduction) ?
             <Report 
-              equivalents={equivalents}
-              carbon={carbon}
-              setApplyReduction={setApplyReduction}
+              result={result}
               reset={reset}
             />
-            :
-            <ReportReduction
-              equivalents={equivalents}
-              reset={reset}
-            />
-          : <Alert severity="error">Could not generated comparisons for these carbon emissions</Alert>}
+          : <Alert severity="error">Could not generate result for this input</Alert>}
         </>
       }
     </Box>
